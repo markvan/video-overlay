b706685fe9f3634fb85c459a4181089c920d4220
markvan <mark@hedtek.com>
2015-06-07 22:25:05 +0100

    close X and trying to improve optimal size

--- google_video.js
+++ google_video.js
@@ -12,18 +12,20 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '80%',
         width: '80%',
+        playerVars: { 'modestbranding': 1},
         videoId: 'M7lc1UVf-VE',
         events: {
-            'onReady': onPlayerReady,
+            'onReady': onPlayerReady
         }
     });
 }
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
+    player.setPlaybackQuality('hd720')
     event.target.playVideo();
 }
 
-$(document).on("click", "#button_1", function () {
+$(document).on("click", "#close_button", function () {
     player.stopVideo();
 });
